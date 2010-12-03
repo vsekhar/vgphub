f7910704c5d6048c91bdf2b61dbcd16ca1e20be3
Vivek Sekhar <vivek@xmain.com>
2010-12-03 03:00:29 -0500

    hacking

--- handler.py
+++ handler.py
@@ -6,6 +6,7 @@
 import os
 import sys
 import logging
+import cgi
 
 from google.appengine.ext import webapp
 from google.appengine.api import users
@@ -23,12 +24,23 @@ class MainPage(webapp.RequestHandler):
 	def get(self):
 		user = users.get_current_user()
 		if user:
-			self.response.headers['Content-Type'] = 'text/plain'
-			self.response.out.write('Hello, %s!' % user.nickname())
+			#self.response.headers['Content-Type'] = 'text/plain'
+			self.response.out.write('<html><body>')
+			if users.is_current_user_admin():
+				self.response.out.write('Hello, Mr. %s!' % user.nickname())
+				self.response.out.write("<a href='%s'>Logout</a>\n" %
+					users.create_logout_url(self.request.uri))
+			else:
+				self.response.out.write('Hello, %s!\n' % user.nickname())
+			content = self.request.get('content')
+			if content:
+				self.response.out.write('%s\n' % cgi.escape(content))
 
 			for key in bucket.list():
 				self.response.out.write(key.name)
 				self.response.out.write(' ')
+			
+			self.response.out.write('</body></html>')
 
 		else:
 			self.redirect(users.create_login_url(self.request.uri))
